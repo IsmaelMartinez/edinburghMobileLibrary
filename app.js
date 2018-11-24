b15a9329db70e7e1c7151937a2eb1e72fd152543
ismaelmartinez <ismaelmartinez@gmail.com>
2018-11-24 00:00:44 +0000

    merging changes from work and home to be able to fetch with cors enabled

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 new Vue({
     el: '#app',
 
@@ -9,89 +7,51 @@ new Vue({
         layers: []
       },
       mounted() {
-        self = this;
-        Papa.parse("https://data.edinburghopendata.info/dataset/03899d22-9d7a-4f84-a2a4-18c576074e8f/resource/5ad304e3-992c-4b9c-8ac9-5f612fcf3c73/download/directoryexport16.csv", {
-            download: true,
-            header: true,
-            complete: function(results) {
-                self.map = L.map('map').setView([55.953251, -3.188267], 11);
-                self.tileLayer = L.tileLayer(
+        this.loadMap();
+      },
+      methods: {
+        async loadMap() {            
+            var self = this;    
+            let blob = await fetch('https://data.edinburghopendata.info/dataset/03899d22-9d7a-4f84-a2a4-18c576074e8f/resource/5ad304e3-992c-4b9c-8ac9-5f612fcf3c73/download/directoryexport16.csv')
+                .then(r => {
+                    console.log(r);
+                    return r.blob()
+                })
+                .then(data =>{
+                    fr = new FileReader();
+
+                    fr.onload = function() {
+                        self.directory = Papa.parse(this.result, {
+                            header: true
+                        });
+                        self.addMap();
+                        self.addMarkers();
+                    };
+
+                    fr.readAsText(data);
+                });
+            },
+            addMap () {
+                this.map = L.map('map').setView([55.953251, -3.188267], 11);
+                this.tileLayer = L.tileLayer(
                 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
                 {
                     maxZoom: 18,
                     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
                 }
                 );
-                self.tileLayer.addTo(self.map);
-                results.data.forEach((el) => {
+                this.tileLayer.addTo(this.map);
+            },
+            addMarkers () {
+                this.directory.data.forEach(el => {
                     if (el['Location']){
                         let popup = el['Name'];
                         if (el['Address']) popup = popup + "<br> " + el['Address'];
                         popup = popup + "<br> "+el['Day and time'];
-                        L.marker(el['Location'].split(',')).addTo(self.map)
+                        L.marker(el['Location'].split(',')).addTo(this.map)
                             .bindPopup(popup);
                     }
                 });
-                //self.csv = results.data;
-                // //L.map(results['Location']
-                // self.data.csv = results;
-                console.log(results.data);
             }
-        });
-        
-      },
-      methods: {
-        // initMap() {
-        //     this.map = L.map('map').setView([55.953251, -3.188267], 12);
-        //     this.tileLayer = L.tileLayer(
-        //     'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
-        //     {
-        //         maxZoom: 18,
-        //         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
-        //     }
-        //     );
-        //     this.tileLayer.addTo(this.map);
-        // },
-        // initLayers() {   
-        //     var self = this;
-        //     Papa.parse("https://data.edinburghopendata.info/dataset/03899d22-9d7a-4f84-a2a4-18c576074e8f/resource/5ad304e3-992c-4b9c-8ac9-5f612fcf3c73/download/directoryexport16.csv", {
-        //         download: true,
-        //         header: true,
-        //         complete: function(results) {
-        //             results.data.forEach(element => {
-        //                 // L.marker("["+ element['Location']+ "]").addTo(self.map)
-        //                 //     .bindPopup(element['Name'])
-        //                 //     .openPopup();
-        //             });
-        //             L.marker([55.953251, -3.188267]).addTo(self.map)
-        //                 .bindPopup('TEst')
-        //                 .openPopup();
-        //             //self.csv = results.data;
-        //             // //L.map(results['Location']
-        //             // self.data.csv = results;
-        //             // console.log(results);
-        //         }
-        //     });
-            
-        //     //parse the data in  POIs
-        // }
-        // convertCSVtoJSON(csv) {
-        //     let lines = csv.split('\n');
-        //     let result =[];
-        //     let headers= linnes[0].split(',');
-
-        //     for (let i=1; i<lines.length;i++) {
-        //         let obj = {};
-        //         let currentline=lines[i].split(",");
-
-        //         for(let j=0;j<headers.length;j++){
-        //             obj[headers[j]] = currentline[j];
-        //         }
-
-        //         result.push(obj);
-        //     }
-
-        //     return JSON.stringify(result);
-        // }
-      },
+        } 
   });
