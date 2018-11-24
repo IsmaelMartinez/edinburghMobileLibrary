6f10ced6502febf062a0eb8c71436f2cd083c9fb
ismaelmartinez <ismaelmartinez@gmail.com>
2018-11-24 00:58:50 +0000

    actually calling the addCurrentLocaiton and changing the icons to use leaflet-color-markers instead of the awesome ones

--- app.js
+++ app.js
@@ -868,6 +868,7 @@ new Vue({
       mounted() {
         this.loadMap();
         this.addMarkers();
+        this.addCurrentLocation();
       },
       methods: {
         loadMap() {
@@ -893,14 +894,20 @@ new Vue({
                 });
             },
             addCurrentLocation() {
-                var redMarker = L.AwesomeMarkers.icon({
-                    icon: 'coffee',
-                    markerColor: 'red'
-                });
+                var greenIcon = new L.Icon({
+                    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                    shadowSize: [41, 41]
+                  });
+                //   
+                //   L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
                       
                 // L.marker([51.941196,4.512291], {icon: redMarker}).addTo(map);
                 navigator.geolocation.getCurrentPosition((position) => {
-                    L.marker([position.coords.latitude,position.coords.longitude], {icon: redMarker})
+                    L.marker([position.coords.latitude,position.coords.longitude], {icon: greenIcon})
                         .addTo(this.map);
                 });
             }
